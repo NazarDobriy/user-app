e8dedc3d25e20c95e8f10f64c2fc0267965a5e39
Nazar <nazaranddobriy@gmail.com>
2024-04-20 19:30:08 +0300

    feat: add click event to custom btn

--- client/src/components/UI/button/Button.tsx
+++ client/src/components/UI/button/Button.tsx
@@ -4,13 +4,18 @@ import classes from "./Button.module.css";
 interface ButtonProps {
   children: ReactNode;
   isPrimary?: boolean;
+  clickHandler?: () => void;
 }
 
-const Button: FC<ButtonProps> = ({ children, isPrimary = true }) => {
+const Button: FC<ButtonProps> = ({
+  children,
+  clickHandler,
+  isPrimary = true
+}) => {
   const buttonClass = isPrimary ? classes.primary : classes.notPrimary;
 
   return (
-    <button className={`${classes.btn} ${buttonClass}`}>
+    <button className={`${classes.btn} ${buttonClass}`} onClick={clickHandler}>
       {children}
     </button>
   );
