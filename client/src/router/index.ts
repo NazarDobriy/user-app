088007fa354f80ad87eed85e192aaffc7c96e8f4
Nazar <nazaranddobriy@gmail.com>
2024-04-21 01:18:27 +0300

    feat: add routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { ComponentType } from "react";
 import Home from "../pages/home/Home";
+import Auth from "../pages/auth/Auth";
 
 export interface IRoute {
   path: string;
@@ -19,5 +20,13 @@ export const publicRoutes: IRoute[] = [
   {
     path: RouteNames.HOME,
     component: Home
+  },
+  {
+    path: RouteNames.LOGIN,
+    component: Auth
+  },
+  {
+    path: RouteNames.SIGN_UP,
+    component: Auth
   }
 ];
