652a79ab9b77732ba89779fa6159e948b0a2bb38
Nazar <nazaranddobriy@gmail.com>
2024-04-21 01:18:51 +0300

    fix: use Navlink

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import classes from "./Navbar.module.css";
 import Button from "../UI/button/Button";
+import { NavLink } from "react-router-dom";
+import { RouteNames } from "../../router";
 
 const Navbar: FC = () => {
   return (
     <nav className={classes.nav}>
       <section className={classes.btns}>
-        <Button>Login</Button>
-        <Button isPrimary={false}>Sign Up</Button>
+        <NavLink to={RouteNames.LOGIN}>
+          <Button>Login</Button>
+        </NavLink>
+        <NavLink to={RouteNames.SIGN_UP}>
+          <Button isPrimary={false}>Sign Up</Button>
+        </NavLink>
       </section>
     </nav>
   );
