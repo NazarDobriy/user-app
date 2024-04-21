a01628931890b5a464fe76626f3e059f9bfd0152
Nazar <nazaranddobriy@gmail.com>
2024-04-21 22:38:36 +0300

    feat: create mock deal data

---
+++ server/src/deal/data/deal.mock.ts
@@ -0,0 +1,44 @@
+import { IDeal } from '../types/deal.interface';
+
+export const mockDeals: IDeal[] = [
+  {
+    id: 1,
+    title: 'The Marina Torch',
+    sum: 6500000,
+    tiket: 60000,
+    yield: 9.25,
+    sold: 75,
+    daysLeft: 150,
+    imgPath: 'http://localhost:5000/images/img_1.png'
+  },
+  {
+    id: 2,
+    title: 'HHHR Tower',
+    sum: 6500000,
+    tiket: 60000,
+    yield: 9.25,
+    sold: 75,
+    daysLeft: 150,
+    imgPath: 'http://localhost:5000/images/img_2.png'
+  },
+  {
+    id: 3,
+    title: 'Ocean peaks',
+    sum: 6500000,
+    tiket: 60000,
+    yield: 9.25,
+    sold: 75,
+    daysLeft: 150,
+    imgPath: 'http://localhost:5000/images/img_3.png'
+  },
+  {
+    id: 4,
+    title: 'AI Yaqoub Tower',
+    sum: 6500000,
+    tiket: 60000,
+    yield: 9.25,
+    sold: 75,
+    daysLeft: 150,
+    imgPath: 'http://localhost:5000/images/img_4.png'
+  }
+];
