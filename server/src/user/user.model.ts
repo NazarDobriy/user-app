ee5a8090049eed99c1aef160764799c1e78d24a7
Nazar <nazaranddobriy@gmail.com>
2024-04-21 19:01:37 +0300

    feat: create user model

---
+++ server/src/user/user.model.ts
@@ -0,0 +1,42 @@
+import { hash } from 'bcrypt';
+import {
+  BeforeCreate,
+  Column,
+  DataType,
+  Model,
+  Table
+} from 'sequelize-typescript';
+
+interface UserCreationAttr {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'User' })
+export class User extends Model<User, UserCreationAttr> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  password: string;
+
+  @BeforeCreate
+  static async hashPassword(user: User): Promise<void> {
+    const hashedPassword = await hash(user.password, 10);
+    user.password = hashedPassword;
+  }
+}
