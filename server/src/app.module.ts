a413bb53d40d3f3b09da7f9e1115048c9391255d
Nazar <nazaranddobriy@gmail.com>
2024-04-21 22:40:15 +0300

    fix: update app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.model';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { DealModule } from './deal/deal.module';
+import { Deal } from './deal/deal.model';
 
 @Module({
   imports: [
@@ -16,10 +20,14 @@ import { User } from './user/user.model';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User],
+      models: [User, Deal],
       autoLoadModels: true
     }),
-    UserModule
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    UserModule,
+    DealModule
   ]
 })
 export class AppModule {}
