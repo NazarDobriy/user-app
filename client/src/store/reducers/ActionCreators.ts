85b0979db423c0735879a99f50f155905e442a29
Nazar <nazaranddobriy@gmail.com>
2024-04-21 19:05:22 +0300

    feat: add action creators for login and create

--- client/src/store/reducers/ActionCreators.ts
+++ client/src/store/reducers/ActionCreators.ts
@@ -1,6 +1,49 @@
+import { AxiosError } from "axios";
+import { $host } from "../../http";
+import { IUser } from "../../models/User";
 import { AppDispatch } from "../store";
 import { authSlice } from "./AuthSlice";
 
-export const changeAuth = () => (dispatch: AppDispatch) => {
-  dispatch(authSlice.actions.changeAuth());
+const USER_API_PATH = "api/user";
+
+export const setIsAuth = (isAuth: boolean) => (dispatch: AppDispatch) => {
+  dispatch(authSlice.actions.setIsAuth(isAuth));
+};
+
+export const createUser = (user: IUser) => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(authSlice.actions.createUser());
+    const response = await $host.post<IUser>(USER_API_PATH, {
+      method: "POST",
+      ...user
+    });
+    dispatch(authSlice.actions.createUserSuccess(response.data));
+  } catch (error) {
+    dispatch(
+      authSlice.actions.createUserFailure(
+        error instanceof AxiosError
+          ? error.response?.data.message
+          : "Create user error"
+      )
+    );
+  }
+};
+
+export const loginUser = (user: IUser) => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(authSlice.actions.loginUser());
+    const response = await $host.post<IUser>(`${USER_API_PATH}/login`, {
+      method: "POST",
+      ...user
+    });
+    dispatch(authSlice.actions.loginUserSuccess(response.data));
+  } catch (error) {
+    dispatch(
+      authSlice.actions.loginUserFailure(
+        error instanceof AxiosError
+          ? error.response?.data.message
+          : "Login user error"
+      )
+    );
+  }
 };
